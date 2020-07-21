ef46fa1e8a79bda9e49d98bc1d51b0e9ca608ac1
ntarun2803 <52375584+ntarun2803@users.noreply.github.com>
2020-07-21 08:54:51 +0530

    bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+#import discord
+
+#client = discord.Client()
+
+""""
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if(message.content.startswith("hello")):
+        await message.channel.send("hello there "  + str(message.author))
+    else:
+        await message.channel.send("hey im still learning cos the guy who created me is an idiot")  
+
+client.run('NzM0Mzg1ODkwMzYwNTU3NjUz.XxRA1Q.Uq5ssqbRGii4NksSmjoDDP5SK_8')
+""""
